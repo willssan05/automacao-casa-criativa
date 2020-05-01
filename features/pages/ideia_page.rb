0bfd2243b33d78508287a8810f1d02ecaf08aaa4
Wilson Barboza Silva Santos <willssan04@gmail.com>
2020-05-01 12:07:12 -0300

    ajuste no projeto

--- features/pages/ideia_page.rb
+++ features/pages/ideia_page.rb
@@ -27,7 +27,7 @@ class Ideia
     end
 
      def clicar_adicionar
-         find(:css, "button[onclick='onOff()']").click
+         find(:xpath, '//*[@id="btnadicionar"]').click
      end
 
     def clicar_salvar
