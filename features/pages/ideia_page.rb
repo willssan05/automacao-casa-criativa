f7d59799091a46dea3f6d5743650cdcc6bbe1218
Wilson Barboza Silva Santos <willssan04@gmail.com>
2020-05-01 09:08:44 -0300

    ajuste no mapeamento da page

--- features/pages/ideia_page.rb
+++ features/pages/ideia_page.rb
@@ -27,7 +27,7 @@ class Ideia
     end
 
     def clicar_adicionar
-        page.find(:css 'button[id=btnadicionar]').click
+        page.find(:css, 'button[id=btnadicionar]').click
     end
 
     def clicar_salvar
